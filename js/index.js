c0ed2ce39de6fa0b3d1eb512201261e9823bbf36
Matt Pahuta <mhpahuta@gmail.com>
2023-07-18 13:42:17 -0700

    Add filtering by employee name, fix grid gap on mobile

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 console.log('Scripts connected!') // debug - remove before production
 
-import employees from "./employees.js";
+import employees from "./employees.js"; // import array of employees
 
 // Get the value of the select option 
 const departmentSelect = document.getElementById('department-select');
 const nameInput = document.getElementById('name-search');
 
 departmentSelect.addEventListener('change', (e) => {
-  console.log('selected: ', e.target.value)
-  console.log(getEmployeesByDepartment(e.target.value))
+  // console.log('selected: ', e.target.value)
+  getEmployeesByTeam(e.target.value); // get employees based on team (string)
 
 })
 
@@ -17,7 +17,7 @@ nameInput.addEventListener('input', filterEmployeesByName);
 
 
 // filter employees by department
-function getEmployeesByDepartment(department) {
+function getEmployeesByTeam(department) { // name of team (string)
   // console.log('All employees: ', employees)
   let selectedEmployees;
   if (department === 'everyone') {
@@ -40,6 +40,9 @@ function filterEmployeesByName(e) {
   const filteredEmployees = employees.filter(employee => {
     return employee.name.toLowerCase().includes(text)
   })
+
+  render(generateEmployeesHtml(filteredEmployees))
+
   console.log(filteredEmployees)
   // return filteredEmployees;
 
@@ -53,8 +56,6 @@ function generateEmployeesHtml(data) {
   // loop over each employee from the filtered data
   for (let employee of data) {
     const { name, title, bio, image, social } = employee;
-    // console.log(social.hasOwnProperty('twitter'))
-    // console.log(social.hasOwnProperty('linkedin'))
 
     // ToDo: a tags and dynamic href's to social media generation
     let socialsHtml = ''
@@ -88,12 +89,6 @@ function generateEmployeesHtml(data) {
   return employeesHtml;
 }
 
-/*
-  Social icon html
-    <img src="./images/linkedin.png" alt="" class="social-icon">
-    <img src="./images/twitter.png" alt="" class="social-icon">
-*/
-
 
 // render the selected employees to the page
 function render(employeesHtml) {
@@ -101,83 +96,4 @@ function render(employeesHtml) {
 }
 
 
-getEmployeesByDepartment('everyone'); // get everyone by default
-
-// Test data
-// const employees = [
-//   {
-//     name: "Jeremy Durant",
-//     title: "CEO",
-//     team: "leadership",
-//     bio: "<p>Jeremy developed an interest in computers at an early age. He graduated with a computer science and electrical engineering degree.</p> <p>He was an early founder of successful companies like BlueSpace, Eazybit, and Drifly.</p>",
-//     image: "jeremy.jpg",
-//     social: {
-//       linkedin: "https://www.linkedin.com/",
-//     },
-//   },
-//   {
-//     name: "Juanita Gonzalez",
-//     title: "COO",
-//     team: "leadership",
-//     bio: "<p>Juanita grew up in Mexico. She later moved to Canada to get her bachelors at McGill and masters in Business Administration at the University of Toronto.</p> <p>Juanita loves her two cats.</p>",
-//     image: "juanita.jpg",
-//     social: {
-//       linkedin: "https://www.linkedin.com/",
-//     },
-//   },
-//   {
-//     name: "Michael Stromburger",
-//     title: "Sales Director",
-//     team: "sales",
-//     bio: "<p>Michael is has been working in sales for the last 25 years.</p> <p>He’s sold insurance, cars, software and much more.</p> <p>He loves to meet new customers.</p>",
-//     image: "michael.jpg",
-//     social: {
-//       linkedin: "https://www.linkedin.com/",
-//     },
-//   },
-//   {
-//     name: "Muhammad Adel",
-//     title: "Customer Success",
-//     team: "customer service",
-//     bio: "<p>Muhammad Adel has worked in customer service for the last 30 years. He's super dedicated.</p> <p>It was his first job after high school and he’s still working in customer service all these later!</p>",
-
-//     image: "muhammad.jpg",
-//     social: {
-//       twitter: "https://twitter.com/",
-//       linkedin: "https://www.linkedin.com/",
-//     },
-//   },
-//   {
-//     name: "Alexander Smith",
-//     title: "Customer Success",
-//     team: "customer service",
-//     bio: "<p>Alexander finished his bachelors from the University of Pennsylvania in Economics.</p> <p>Alexander loves skateboarding, cycling, drinking beer and snowboarding.</p>",
-
-//     image: "alexander.jpg",
-//     social: {
-//       linkedin: "https://www.linkedin.com/",
-//     },
-//   },
-//   {
-//     name: "Liu Zhang",
-//     title: "Software Engineer",
-//     team: "product",
-//     bio: "<p>Liu grew up in New York. She later moved to Miami to get her bachelors at FIU and masters in Business Administration at the University of Miami.</p> <p>Liu loves her two pugs.</p>",
-//     image: "liu.jpg",
-//     social: {
-//       twitter: "https://twitter.com/",
-//     },
-//   },
-//   {
-//     name: "Jessica Albertson",
-//     title: "Designer",
-//     team: "product",
-//     bio: "<p>Jessica did a double major in Art and Industrial Design at Carnegie Mellon University.</p> <p>She currently lives in Philadelphia  with her husband Sej and two kids.</p>",
-//     image: "jessica.jpg",
-//     social: {
-//       twitter: "https://twitter.com/",
-//       linkedin: "https://www.linkedin.com/",
-//     },
-//   },
-
-// ]
+getEmployeesByTeam('everyone'); // get everyone by default
